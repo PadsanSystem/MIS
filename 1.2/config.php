c25039fa1e8a5635391876dd2c7dada93fe24f7c
Ehsan Mahmoodi <mahmoodi@padsansystem.ir>
2014-07-18 16:49:27 +0430

    Copyright
    add copyright comment

--- 1.2/config.php
+++ 1.2/config.php
@@ -1,4 +1,11 @@
-<?php 
+<?php
+/* Management Information System (MIS)
+ * Design & Development By AtfaGroup
+ * Website : www.atfagroup.com
+ * Email : info@atfagroup.com
+ * Tel : 026-32545700
+ * Fax : 026-32545701
+ */
 $server_host="localhost";
 $server_username="root";
 $server_password="";
