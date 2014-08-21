919c416af6b1776ed63430cba73a35883a4ef818
Ehsan Mahmoodi <mahmoodi@padsansystem.ir>
2014-08-21 20:30:31 +0430

    comments

--- 1.2/index.php
+++ 1.2/index.php
@@ -1,7 +1,7 @@
 <?php
 /* Management Information System (MIS)
  * Design & Development By AtfaGroup
- * Website : www.atfagroup.com
+ * Website : www.atfagroup.coms
  * Email : info@atfagroup.com
  * Tel : 026-32545700
  * Fax : 026-32545701
